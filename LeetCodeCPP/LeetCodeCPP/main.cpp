eed43905f483d51dd331da9d2a15dd1cd12b3587
admin <guodongliu6@crediteses.cn>
2019-02-20 14:37:22 +0800

    main  code

---
+++ LeetCodeCPP/LeetCodeCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCodeCPP
+//
+//  Created by admin on 2019/2/20.
+//  Copyright © 2019年 liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World fucks!\n";
+    return 0;
+}
