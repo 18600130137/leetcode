3eae005794e6c6bcfcf69facd8314493c002c7ef
admin <guodongliu6@crediteses.cn>
2019-03-07 14:43:19 +0800

    32 again

--- LeetCodeCPP/32. Longest Valid Parentheses/main.cpp
+++ LeetCodeCPP/32. Longest Valid Parentheses/main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <stack>
+#include <vector>
 using namespace std;
 
 auto speed=[]()
@@ -18,9 +19,12 @@ auto speed=[]()
     return nullptr;
 }();
 
+
+
 class Solution {
 public:
-    int longestValidParentheses(string s) {
+    //O(2n)
+    int longestValidParentheses1(string s) {
         stack<int>  helper;
         int m=s.size();
         int longest=0;
@@ -53,8 +57,23 @@ public:
         }
         return longest;
     }
+    
+    //O(n)  dp solution
+    int longestValidParentheses(string s) {
+        stack<int>  helper;
+        int m=s.size();
+        vector<int>  longest(m,0);
+        int ret=0;
+        for(int i=1;i<m;i++){
+            if(s[i]==')' && i-longest[i-1]-1>=0 && s[ i-longest[i-1]-1]=='('){
+                longest[i]=longest[i-1]+2+(i-longest[i-1]-2>=0?longest[i-longest[i-1]-2]:0);
+                ret=max(ret,longest[i]);
+            }
+        }
+        return ret;
+    }
 };
-
+//"()(()"
 int main(int argc, const char * argv[]) {
     string input="((()()";
     Solution so=Solution();
